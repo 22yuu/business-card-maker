25cb225575ac8a0dbd754812718f3f59dc96562e
이진유 <22yuu@naver.com>
2022-03-01 20:41:34 +0900

    add login and logout functionality using firebase

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,15 @@
+import { Routes, Route } from "react-router-dom";
 import styles from "./app.module.css";
 import Login from "./components/login/login";
+import Maker from "./components/maker/maker";
 
 function App({ authService }) {
   return (
     <div className={styles.app}>
-      <Login authService={authService} />;
+      <Routes>
+        <Route path="/" element={<Login authService={authService} />} />
+        <Route path="/maker" element={<Maker authService={authService} />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.module.css";
 import App from "./app";
 import AuthService from "./service/auth_service";
+import { BrowserRouter } from "react-router-dom";
 
 const authService = new AuthService();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App authService={authService} />
+    <BrowserRouter>
+      <App authService={authService} />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
