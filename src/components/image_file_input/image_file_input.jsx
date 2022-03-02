2e5bf0c41c61c5eed3734530c638258750c9f827
이진유 <22yuu@naver.com>
2022-03-02 23:12:58 +0900

    handle image uploading

--- src/components/image_file_input/image_file_input.jsx
+++ src/components/image_file_input/image_file_input.jsx
@@ -1,6 +1,38 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./image_file_input.module.css";
 
-const ImageFileInput = (props) => <button>Image</button>;
+const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
+  const inputRef = useRef();
+  const onButtonClick = (event) => {
+    event.preventDefault();
+    inputRef.current.click();
+  };
+
+  const onChange = async (event) => {
+    console.log(event.target.files[0]);
+    const uploaded = await imageUploader.upload(event.target.files[0]);
+    console.log(uploaded);
+    // upload 함수가 실행될 때까지 기다렸다가 업로드되면 변수에 할당
+    onFileChange({
+      name: uploaded.original_filename,
+      url: uploaded.url,
+    });
+  };
+  return (
+    <div className={styles.container}>
+      <input
+        ref={inputRef}
+        className={styles.input}
+        type="file"
+        accept="image/*"
+        name="file"
+        onChange={onChange}
+      />
+      <button className={styles.button} onClick={onButtonClick}>
+        {name || "No File"}
+      </button>
+    </div>
+  );
+};
 
 export default ImageFileInput;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,24 @@ import "./index.module.css";
 import App from "./app";
 import AuthService from "./service/auth_service";
 import { BrowserRouter } from "react-router-dom";
+import ImageUploader from "./service/image_uploader";
+import ImageFileInput from "./components/image_file_input/image_file_input";
 
 const authService = new AuthService();
+const imageUploader = new ImageUploader();
+const FileInput = (props) => (
+  // props 를 전달 받은 이유는 onClick, onChange 등 컴포넌트의 다른 prop을 활용하기 위함이다.
+  // 즉 컴포넌트의 확장성을 위하여 props를 전달 받는다.
+  <ImageFileInput {...props} imageUploader={imageUploader} />
+);
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App authService={authService} />
+      <App authService={authService} FileInput={FileInput} />
+      {/* FileInput이라는 컴포넌트를 따로 만들어 prop으로 전달해준 이유는 authService 같은 Defendency Injection이 필요한
+          경우 위의 컴포넌트에만 추가해주면 바로 적용이 가능하다.
+      */}
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
